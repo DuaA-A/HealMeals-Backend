291190a7a9f9db6a6f6cd89c4c81e09396e7aa7e
Duaa AbdelAti AbdelAzeem <164356690+DuaA-A@users.noreply.github.com>
2025-09-14 23:59:34 +0300

    update the recipe later

--- Api/src/main/java/HealMeals/Api/Controller/UserController.java
+++ Api/src/main/java/HealMeals/Api/Controller/UserController.java
@@ -1,15 +1,13 @@
 package HealMeals.Api.Controller;
 
-
 import HealMeals.Api.DTO.UserDTO;
 import HealMeals.Api.service.UserService;
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/users")
@@ -26,5 +24,21 @@ public class UserController {
     @GetMapping
     public ResponseEntity<List<UserDTO>> getAll() {
         return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getById(@PathVariable UUID id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDTO> update(@PathVariable UUID id, @RequestBody UserDTO dto) {
+        return ResponseEntity.ok(userService.updateUser(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
     }
 }
